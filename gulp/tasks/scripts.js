c7921a2639db369419e3c4e419dc2fd123f14789
olivier <olivier@mediainvest.io>
2016-07-13 16:14:38 +0200

    update

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -17,56 +17,6 @@ uglify = require('gulp-uglify')
 stripComments = require('gulp-strip-comments')
 buildHelper = require('../helpers/build-helper');
 
-gulp.task('polymer-js', function () {
-    gulp.src(["bower_components/polymer/polymer-micro.html", "bower_components/polymer/polymer-mini.html", "bower_components/polymer/polymer.html"])
-        .pipe(extract({
-            sel: "script"
-        }))
-        .pipe(addsrc('bower_components/polymer-ts/polymer-ts.js'))
-        .pipe(stripComments())
-        .pipe(uglify())
-        .pipe($.concat('polymer.js'))
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('platform-js', function () {
-    gulp.src(["bower_components/webcomponentsjs/webcomponents.min.js", "dist/polymer.js", "bower_components/jquery.namespace/jquery.namespace.js", "bower_components/javascript-auto-complete/auto-complete.js"])
-        .pipe(stripComments())
-        // .pipe(uglify())
-        .pipe($.concat('platform.js'))
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('app-js', function () {
-    var $return = gulp.src(['src/*.ts', 'src/**/*.ts'])
-        .pipe(sourcemaps.init())
-        .pipe(ts(tsProject))
-        .pipe(babel())
-        .pipe($.concat('app.js'))
-        .pipe(wrapper({
-            header: "(function ($) {addEventListener('WebComponentsReady', function () {\n",
-            footer: '});})(jQuery);'
-        }))
-        .pipe(stripComments())
-        //.pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'));
-
-    if (buildHelper.isRelease()) {
-
-    }
-
-    return $return;
-});
-
-gulp.task('build-js', function () {
-    gulp.src(["dist/platform.js", "dist/app.js"])
-        .pipe(stripComments())
-        //.pipe(uglify())
-        .pipe($.concat('build.js'))
-        .pipe(gulp.dest('dist'));
-});
-
 
 gulp.task('copy', function () {
     gulp.src([
@@ -118,15 +68,16 @@ gulp.task('threeds-landingpage-js', function () {
         .pipe(addsrc.prepend('bower_components/jquery-file-download/src/Scripts/jquery.fileDownload.js'))
         .pipe($.concat('threeds.landingpage.js'))
         .pipe(stripComments())
-        //.pipe(uglify())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist'));
 
     if (buildHelper.isRelease()) {
-
+        $return.pipe(uglify())
+            .pipe($.concat('threeds.landingpage.min.js'))
+            .pipe(gulp.dest('dist'));
     }
 
-    return $return;
+    return $return
 });
 
 
