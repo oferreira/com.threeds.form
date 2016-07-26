d065060d64fa6cc0df1969448ca6afbdf720d9e7
olivier <olivier@mediainvest.io>
2016-07-26 14:04:27 +0200

    translate

--- src/Locale.ts
+++ src/Locale.ts
@@ -10,10 +10,10 @@ namespace Com.Threeds {
         },
         'fr': {
             'error': {
-                'field_invalid':`This field is invalid`,
-                'field_require':'This field is required',
-                'email_invalid':'A valid email address is required',
-                'checkbox_require':'Please select the check box',
+                'field_invalid':`Ce champ est invalide`,
+                'field_require':'Ce champ est requis',
+                'email_invalid':'L\'adresse email entrer est invalide',
+                'checkbox_require':'S\'il vous plaît merci de sélectionner la case à cocher',
             },
         }
     };
